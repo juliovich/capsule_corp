60d737c307a899561893740df37ab3788f7efc04
clement duchemin <clement_mz@protonmail.com>
2018-05-15 12:04:24 +0200

    add column name to robots

---
+++ db/migrate/20180515100034_add_name_to_robots.rb
@@ -0,0 +1,4 @@
+class AddNameToRobots < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_091433) do
+ActiveRecord::Schema.define(version: 2018_05_15_100034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
